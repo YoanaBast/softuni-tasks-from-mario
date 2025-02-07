3295e3511853307cf3c79ee86fc2066e08d7abd5
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-02-07 17:08:37 +0200

    removed unrelated files

--- functions/repeat_string.py
+++
@@ -1,7 +0,0 @@
-repeat_string = lambda string, n: string * n
-
-input_string = input()
-counter = int(input())
-result = repeat_string(input_string, counter)
-
-print(result)
--- list_basic/search.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-word = input()
-
-strings = [input() for _ in range(n)]
-filtered_strings = [current_string for current_string in strings if word in current_string]
-
-print(strings)
-print(filtered_strings)
