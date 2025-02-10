9a7ee7225f2216e0e92f952c30146e740f87de79
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-02-10 17:19:06 +0200

    removing unrelated

--- data_types_and_variables/concatenate_names.py
+++
@@ -1,5 +0,0 @@
-first_name = input()
-last_name = input()
-sign = input()
-
-print(f'{first_name}{sign}{last_name}')
--- data_types_and_variables/pounds_to_dollars.py
+++
@@ -1,5 +0,0 @@
-amount = int(input())
-british_pound = 1.31
-convert_pounds_to_usd_dollars = amount * british_pound
-
-print(f'{convert_pounds_to_usd_dollars:.3f}')
